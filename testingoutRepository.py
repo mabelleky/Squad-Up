275755dc6f39f39b3de4cf30102c90772c708261
Alfred-Langer-1999 <Alfred-Langer1999-@users.noreply.github.com>
2019-10-02 15:00:22 -0400

    Create testingoutRepository.py

---
+++ testingoutRepository.py
@@ -0,0 +1,17 @@
+def creator(a,b,c,d):
+    print ("The four creators of this project are:")
+    print (a)
+    print (b)
+    print (c)
+    print (d)
+    print ("Now here is some extra spam)
+    for i in range(0,30):
+        print (a)
+        print (b)
+        print (c)
+        print (d)
+        
+    return "My pp is wee wee"
+
+
+print(creator("Mabel","Steven","Hassaan","Alfred")
